3a6ae59137267a6430070230041e4747a51791b8
Jan Malý <x526325@fi.muni.cz>
2026-04-22 16:39:05 +0200

    chore

--- diploma_thesis/utils/upload_to_drive.py
+++ diploma_thesis/utils/upload_to_drive.py
@@ -16,11 +16,11 @@ drive_service = build("drive", "v3", credentials=credentials)
 
 def upload_json_to_drive(data: list | dict, file_name: str) -> None:
     """
-    Serializuje data do JSONu a nahraje je na Google Drive.
+    Serialize to JSON, upload to Google Drive, and share the file with the email address from settings.
 
     Args:
-        data (dict | list): Data, která chcete uložit.
-        file_name (str): Název souboru, pod kterým se uloží na Disku.
+        data (dict | list): Data to save.
+        file_name (str): File name under which the data saves to the Drive.
     """
     if type(data) is dict:
         data = [data]
@@ -53,9 +53,9 @@ def upload_json_to_drive(data: list | dict, file_name: str) -> None:
         drive_service.permissions().create(fileId=file.get("id"), body=permission).execute()
 
     except Exception as e:
-        logger.exception(f"Error uploading file ({file_name} to Drive")
+        logger.exception(f"Error uploading file ({file_name}) to the Drive: {e}")
 
 
 if __name__ == '__main__':
-    data = ["list", "list", {"key": "value"}]
+    data = ["string", "string", {"key": "value"}]
     upload_json_to_drive(data, "test.json")
